7e80f9c030cca1a6a99b67a062db72cb6a9a85a8
Duc Toan <22120376@student.hcmus.edu.vn>
2024-12-11 13:58:29 +0700

    update seed

--- seeds/DemoSeed.js
+++ seeds/DemoSeed.js
@@ -7,40 +7,38 @@ exports.seed = async function(knex) {
   await knex('Doctor').del();
   await knex('Specialty').del();
   await knex('EndUser').del();
+  await knex('Medicine').del();
 
   const [admin, staff, doctor] = await knex('EndUser').insert([
     {
-      name: 'admin',
+      name: 'Admin',
       role: 'admin',
       username: 'admin',
-      password: 'AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAAqfvU7w59lEKDIIjma38xOwAAAAACAAAAAAAQZgAAAAEAACAAAABGU2rkbMqb6Mshh72/cymk8e/X+bK0+k0a3RGTWsYAfQAAAAAOgAAAAAIAACAAAABUCb5xqheZ9UC57bfOsKXtCxYaD9MUtL20NLmViKR9lRAAAABNyyK8XuuaDSYNni4GDoEgQAAAAAELXJrtvOUTEx2Q8y0IAxdI9gMYdx9rLjeQjnbiPZ49/ZRSh0a7BT7PFF1SOQwkhFOA8Vo4lnm3iEgQDVfAUZs=',
-      phone: '0909090909',
-      birthday: '2000-10-27',
+      password: '$2a$11$b7pJOs6WuMGldTBkFdX6E.f0z.PACOYfQdNpt99yk/abbRaN.4xnG',
+      phone: '0987654321 ',
+      birthday: '2000-10-10',
       address: 'HCM',
-      gender: 'Male',
-      entropy: 'iwN01SQcP7r4xSKKvf1G2NYvYSI='
+      gender: 'Male'
     },
     {
       name: 'staff',
       role: 'staff',
-      username: 'staff',
-      password: 'AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAAqfvU7w59lEKDIIjma38xOwAAAAACAAAAAAAQZgAAAAEAACAAAAAItJb2sqifgO3qhQUqKxzHmKK5VpiQxcQoFvwTkb/GyQAAAAAOgAAAAAIAACAAAAAZoEpNwH1BfAQZwUGb416lbECC7r8FYMDptIIbID/jdhAAAACE/JWG8qdiK1gqJeCSV+umQAAAAGrBxMXRErRPSKjrXyASwIA8cdSpkfxZnlrVoyzbGJgUfWA97egTTgR21vB0VCIoCB9EnmszbDTRoIh9ZNgMIeg=',
+      username: 'staff5',
+      password: '$2a$11$wkg.taffxC/kIiggLSUgZezk90smQy4RnU6/pDdE.7IfJ2inNg6FW',
       phone: '0808080808',
       birthday: '2000-01-01',
       address: 'HCM',
-      gender: 'Male',
-      entropy: 'h9+ep4dvQ1jxwIM+rGtfB+oM2xY='
+      gender: 'Male'
     },
     {
       name: 'doctor',
       role: 'doctor',
       username: 'doctor',
-      password: 'AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAAqfvU7w59lEKDIIjma38xOwAAAAACAAAAAAAQZgAAAAEAACAAAADj9veshIqbs/u0/sE9GETnMGEhEvZArov8LQP1VNkY0AAAAAAOgAAAAAIAACAAAABMKiujttbB9LPzPWbBV6QiqlWtO/nGTuzDAFgFZaTbhRAAAACfMwc7amqgg+jI5CdKgwRIQAAAAFWWW7dYKrEBd1D7meLej1bh/awnj5ey1A/WtNgAu3pZrYOd+TDEGsc1x5qEK6am4iZPTsRqeeB5QLYpbr+DpT0=',
+      password: '$2a$11$lj5hCZ3gPFQ4xhC8F5a8keT7rieJl3soBuLFlqRoV0.Ev6viXJsua',
       phone: '0707070707',
       birthday: '1999-01-01',
       address: 'HCM',
-      gender: 'Female',
-      entropy: 'GW7EdtHGjqjEz2ETlVyCKWXA3RY='
+      gender: 'Female'
     }
   ]).returning('id'); 
 
@@ -51,17 +49,30 @@ exports.seed = async function(knex) {
 
   const [specialty] = await knex('Specialty').insert([
     { name: "Khoa 1" },
+    { name: "Khoa 2" },
+    { name: "Khoa 3" },
+    { name: "Khoa 4" }
   ]).returning('id'); 
-
 
   const specialtyId = specialty.id;
 
-
+  
+  const medicine = await knex('Medicine').insert([
+    { name: "Paracetamol", manufacturer: "Long Châu", price: 10000, quantity: 100, ExpDate: "2024-05-05", MfgDate: "2025-05-05" },
+    { name: "Panadol", manufacturer: "FPT", price: 12000, quantity: 100, ExpDate: "2024-07-05", MfgDate: "2025-07-05" },
+    { name: "Alexan", manufacturer: "Dược Hậu Giang", price: 9000, quantity: 100, ExpDate: "2024-10-05", MfgDate: "2025-10-05" },
+    { name: "Azental", manufacturer: "Dược Long An", price: 14000, quantity: 100, ExpDate: "2024-12-08", MfgDate: "2025-12-08" },
+    { name: "Furgacar", manufacturer: "Dược Cần Thơ", price: 17000, quantity: 100, ExpDate: "2024-10-10", MfgDate: "2025-10-10" },
+    { name: "Azotel", manufacturer: "Dược Bạc Liêu", price: 15000, quantity: 100, ExpDate: "2024-06-10", MfgDate: "2025-06-10" },
+    
+  ]);
+  
+  
   await knex('Doctor').insert([
     {
       userId: userIdDoctor, 
       specialtyId: specialtyId, 
-      room: 'P1',
+      room: 'P101',
     },
   ]);
 };
